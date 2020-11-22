874637eceb50808c4be11a37b908b506daa9dc99
Apolok99 <strakerzakry@gmail.com>
2020-11-22 19:37:12 +0100

    render Index to index

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router(); // creem una miniaplicacio de aquesta ruta
 
 // HTTP GET
 router.get('/', (req, res) => {
-    res.render('Index');
+    res.render('index');
 });
 
 module.exports = router;
